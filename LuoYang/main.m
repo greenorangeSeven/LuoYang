d75409e8f0d69cdf7aab349169a84f306c78f744
greenorangeCN <Seven@SevendeMacBook-Pro.local>
2014-09-17 09:50:50 +0800

    Initial Commit

---
+++ LuoYang/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LuoYang
+//
+//  Created by Seven on 14-9-17.
+//  Copyright (c) 2014年 greenorange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
